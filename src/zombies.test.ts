7346b3663188693f695b00101aa588539a392c27
Odin Thif <mjd.thif@icloud.com>
2024-09-09 15:06:36 +0200

    added test | one-roomer becomes full when a zombie is added

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -8,28 +8,38 @@ const createRoom = (capacity: number) => {
     isFull: () => true,
     notFull: () => false,
     isEmpty: (_capacity: number) => (_capacity >= 2 ? true : false),
+    addZombie: (_capacity: number) => (_capacity === 1 ? _capacity-- : false),
   };
 };
 
 test("room is not full", () => {
   const roomCapacity = createRoom(0);
   const result = roomCapacity.isFull();
+
   ok(result);
 });
 
 test("empty room that fits one zombie is not full", () => {
   const roomCapacity = createRoom(1);
   const result = roomCapacity.notFull();
+
   ok(!result);
 });
 
 test("empty room cannot fit any zombies", () => {
+  // if room capacity is 2 or more(meaning no zombies inside) then the room is empty and therefor can not fit any zombies.
   const roomCapacity = createRoom(2);
   const result = roomCapacity.isEmpty(2);
+
   ok(result);
 });
 
-// test.skip("one-roomer becomes full when a zombie is added", () => {});
+test("one-roomer becomes full when a zombie is added", () => {
+  const roomCapacity = createRoom(1);
+  const result = roomCapacity.addZombie(1);
+
+  ok(result);
+});
 
 // test.skip("two-roomer is not full when a zombie is added", () => {});
 
